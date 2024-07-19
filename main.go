38628acdbc7e8541c1277addb8559a0623c150a2
kabasunny <dxbqd553@yahoo.co.jp>
2024-07-19 16:09:17 +0900

    4 commit

--- main.go
+++ main.go
@@ -24,5 +24,7 @@ func main() {
 	r.GET("/items", itemController.FindAll)
 	r.GET("/items/:id", itemController.FindById)
 	r.POST("/items", itemController.Create)
+	r.PUT("/items/:id", itemController.Update)
+	r.DELETE("/items/:id", itemController.Delete)
 	r.Run("localhost:8080")
 }
